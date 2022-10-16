be75ab81e09c67aebeb2841de4bbe49c5117b872
Korolev <romakorolev9702@gmail.com>
2022-10-16 19:15:32 +0500

    Design

---
+++ Prac/app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.practice
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
