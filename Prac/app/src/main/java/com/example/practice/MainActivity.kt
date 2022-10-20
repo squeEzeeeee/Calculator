f3dcd1d5c8f4e3e4448ae8b00f6bcfe4e1d3695a
Korolev <romakorolev9702@gmail.com>
2022-10-20 18:55:37 +0300

    buttons

--- Prac/app/src/main/java/com/example/practice/MainActivity.kt
+++ Prac/app/src/main/java/com/example/practice/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.practice
 
-import android.graphics.Color
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +11,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_0.setOnClickListener { setTextFields("0") }
+        btn_1.setOnClickListener { setTextFields("1") }
+        btn_2.setOnClickListener { setTextFields("2") }
+        btn_3.setOnClickListener { setTextFields("3") }
+        btn_4.setOnClickListener { setTextFields("4") }
+        btn_5.setOnClickListener { setTextFields("5") }
+        btn_6.setOnClickListener { setTextFields("6") }
+        btn_7.setOnClickListener { setTextFields("7") }
+        btn_8.setOnClickListener { setTextFields("8") }
+        btn_9.setOnClickListener { setTextFields("9") }
+        sum_btn.setOnClickListener { setTextFields("+") }
+        min_btn.setOnClickListener { setTextFields("-") }
+        divide_btn.setOnClickListener { setTextFields("/") }
+        multiply_btn.setOnClickListener { setTextFields("*") }
 
+        clear_btn.setOnClickListener {
+            math_operation.text= ""
+            result.text=""
+        }
+
+        bck_btn.setOnClickListener {
+            val str=math_operation.text.toString()
+            if(str.isNotEmpty())
+                math_operation.text= str.substring(0,str.length-1)
+        }
+
+    }
+
+    fun setTextFields(str: String) {
+    math_operation.append(str)
     }
 }
